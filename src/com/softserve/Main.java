fd752a65ec8beff3e24f3f7ea5d604b97b565ccf
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-18 15:29:32 +0300

    Add files via upload

---
+++ src/com/softserve/Main.java
@@ -0,0 +1,23 @@
+package com.softserve;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        int result;
+
+        /* code */
+        if (a > b) {
+            result = 1;
+        } else if (a < b) {
+                result = -1;
+            } else {
+            // if (a == b) {
+                    result = 0;
+                }
+        System.out.println(result);
+    }
+  }
+
+    
+
